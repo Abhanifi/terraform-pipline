18138e59a30d47a08b3866f695fa9c2d6e2e979b
NewUserName <ahanafi@akumoproject.com>
2025-06-12 23:01:41 -0500

    updated

--- main.tf
+++ main.tf
@@ -3,6 +3,8 @@ resource "aws_instance" "jenkins_demo" {
   instance_type = var.instance_type
   count         = 2
 
+  disable_api_termination = true
+
   tags = {
     Name = format("akumosolutions-%03d", count.index + 1)
   }
